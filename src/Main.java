9d018950d82b3cbb7d62dbd5f5e3be522d67fee1
orszulakana <orszulakana@gmail.com>
2018-02-11 10:36:14 +0100

    Add test stack method

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,26 @@ import datastructures.Stack;
 public class Main {
 
     public static void main(String[] args) {
-        Stack stack = new Stack();
+        testStack();
+    }
 
-        
+    private static void testStack(){
+        Stack stack = new Stack();
+        stack.push(2);
+        stack.show();
+
+        stack.push(6);
+        stack.push(5);
+        stack.show();
+
+        stack.pop();
+        stack.show();
+
+        stack.pop();
+        stack.show();
+    }
+
+    private static  void testQueue(){
+
     }
 }
